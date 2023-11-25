f2abacc70402be7c44a7aa40cd33a67fc5e2fe37
JannaturNur08 <nur@playense.com>
2023-11-25 15:10:33 +0600

    patch api created for making moderator and admin for users

--- index.js
+++ index.js
@@ -1,79 +1,103 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oh6dvsr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-  
-    const userCollection = client.db('FinalProject').collection('users');
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
 
+		const userCollection = client.db("FinalProject").collection("users");
 
-    // users api
-    
-    app.get('/users', async(req,res)=> {
-      const result = await userCollection.find().toArray();
-      res.send(result);
-    })
+		// users api
 
-    app.post ('/users', async(req,res) => {
-      const user = req.body;
-      // to check existing email
-      const query = {email: user.email};
-      const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-         return  res.send({ message: 'User already exists', insertedId: null});
-      }
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    })
+		app.get("/users", async (req, res) => {
+			const result = await userCollection.find().toArray();
+			res.send(result);
+		});
 
-    app.delete("/users/:id", async (req, res) => {
+		app.post("/users", async (req, res) => {
+			const user = req.body;
+			// to check existing email
+			const query = { email: user.email };
+			const existingUser = await userCollection.findOne(query);
+			if (existingUser) {
+				return res.send({
+					message: "User already exists",
+					insertedId: null,
+				});
+			}
+			const result = await userCollection.insertOne(user);
+			res.send(result);
+		});
+
+    //make moderator
+		app.patch("/users/moderator/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const updatedDoc = {
+				$set: {
+					role: "moderator",
+				},
+			};
+			const result = await userCollection.updateOne(query, updatedDoc);
+			res.send(result);
+		});
+    //make admin
+		app.patch("/users/admin/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const updatedDoc = {
+				$set: {
+					role: "admin",
+				},
+			};
+			const result = await userCollection.updateOne(query, updatedDoc);
+			res.send(result);
+		});
+
+    //user delete by admin
+		app.delete("/users/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const result = await userCollection.deleteOne(query);
 			res.send(result);
 		});
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-   // await client.close();
-  }
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Final Effort is running!')
-})
+app.get("/", (req, res) => {
+	res.send("Final Effort is running!");
+});
 
 app.listen(port, () => {
-  console.log(`Final app listening on port ${port}`) 
-})
+	console.log(`Final app listening on port ${port}`);
+});
