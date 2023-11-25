81408a8741078ba9c8c55f3294aede8950c3b6d6
JannaturNur08 <nur@playense.com>
2023-11-25 14:54:01 +0600

    users delete api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -50,6 +50,13 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+
+    app.delete("/users/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await userCollection.deleteOne(query);
+			res.send(result);
+		});
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
